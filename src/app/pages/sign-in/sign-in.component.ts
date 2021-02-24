800816c07cd650fe2121cbda088e25fbc6a8af07
Your Name <you@example.com>
2021-02-24 16:51:56 +0100

    Add Register And Login

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material';
 import { emailValidator, matchingPasswords } from '../../theme/utils/app-validators';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -13,32 +14,40 @@ export class SignInComponent implements OnInit {
   loginForm: FormGroup;
   registerForm: FormGroup;
 
-  constructor(public formBuilder: FormBuilder, public router:Router, public snackBar: MatSnackBar) { }
+  constructor(public router:Router, public snackBar: MatSnackBar, private authService: AuthService) { }
 
   ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      'email': ['', Validators.compose([Validators.required, emailValidator])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(6)])] 
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.compose([Validators.required, emailValidator])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)]))
     });
 
-    this.registerForm = this.formBuilder.group({
-      'name': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      'email': ['', Validators.compose([Validators.required, emailValidator])],
-      'password': ['', Validators.required],
-      'confirmPassword': ['', Validators.required]
-    },{validator: matchingPasswords('password', 'confirmPassword')});
+    this.registerForm = new FormGroup({
+      name: new FormControl('',Validators.compose([Validators.required, Validators.minLength(3)]) ) ,
+      email:new FormControl('',Validators.compose([Validators.required, emailValidator])),
+      password: new FormControl('', Validators.required),
+      confirmPassword :new FormControl('', Validators.required) 
+    })
+    // {validator: matchingPasswords('password', 'confirmPassword')});
 
   }
 
   public onLoginFormSubmit(values:Object):void {
     if (this.loginForm.valid) {
-      this.router.navigate(['/']);
+      this.authService.login(this.loginForm.value).subscribe((res)=>{
+        this.router.navigate(['/']);
+      })
     }
   }
 
+
+
   public onRegisterFormSubmit(values:Object):void {
     if (this.registerForm.valid) {
-      this.snackBar.open('You registered successfully!', '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
+      this.authService.register(this.registerForm.value).subscribe((res)=>{
+        this.snackBar.open('You registered successfully!', '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
+      })
+      
     }
   }
 
