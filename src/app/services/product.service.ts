4a2437a4d1500a429401c1853a0ad8f98397e926
Mahmoudfad <you@example.commahmoudfadhel2@gmail.com>
2021-03-03 10:55:34 +0100

    add image

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+   baseURL= environment.baseURL
+  constructor(public http:HttpClient) { }
+
+
+ 
+
+  addProduct(data)
+  {
+    return this.http.post( this.baseURL + '/products/addProduct',data)
+  }
+
+}
